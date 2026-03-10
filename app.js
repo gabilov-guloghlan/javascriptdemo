b398348924cb21035c8b9b86b10c84c9dc10b281
gabilov-guloghlan <gabilovguloglan@gmail.com>
2026-03-10 17:38:59 +0000

    asmet6

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
 "use strict";
 $(document).ready(function () {
 
-$('.btn').click(function (e) { 
-    e.preventDefault();
-    let a = (a) => {
-        for (let i = 1; i <=a; i++)
-            $('.col-md-12').append(`<h2>${i}</h2>`);
-    }
-    a($('input').val());
-});
-    
+    $('.btn').click(function (e) {
+        e.preventDefault();
+
+        let a = $('input').val();
+
+        
+
+        if (!isNaN(a)) {
+
+            for (let i = 1; i <= a; i++) {
+                $('.miniboxes').append(`
+                    <div class="minibox d-flex justify-content-center align-items-center mt-3">
+                        <h2>${a}</h2>
+                    </div>
+                `);
+            }
+
+        } else {
+
+            $('.miniboxes').append(`
+                <div class="minibox d-flex justify-content-center align-items-center mt-3">
+                    <h2>${a}</h2>
+                </div>
+            `);
+
+        }
+
+    });
+
 });
